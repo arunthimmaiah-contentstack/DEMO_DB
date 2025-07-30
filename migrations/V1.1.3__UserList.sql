46192b64a53e5ce4e7d897e0d84e70beea024de8
arunthimmaiah-contentstack <arun.thimmaiah@contentstack.com>
2025-07-30 06:39:54 +0530

    Update V1.1.3__UserList.sql

--- migrations/V1.1.3__UserList.sql
+++ migrations/V1.1.3__UserList.sql
@@ -4,7 +4,7 @@ CREATE OR REPLACE TABLE development.demo.users_list (
     email STRING,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-INSERT INTO development.dev_demo.users_list (id, name, email)
+INSERT INTO development.demo.users_list (id, name, email)
 VALUES 
     (1, 'Alice', 'alice@example.com'),
     (2, 'Bob', 'bob@example.com');
